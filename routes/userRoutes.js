43b3e33c3bc5e57dcd609bf379a7bf24376d2b4a
falcainterns <arjunsingh7743@gmail.com>
2024-01-05 18:34:49 +0530

    user controller and some changes

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser,
+    loginUser,
+    currentUser } = require("../controllers/userController")
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/current", currentUser);
+module.exports = router;
